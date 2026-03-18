6aadfb7a763af4cfc2fff6d3025a47980f5f4a0c
Lilian CHAUVET <poussincolossal@gmail.com>
2026-03-18 15:51:50 +0100

    Created activitys Option and Credits

--- app/src/main/java/com/example/spaceshooters/MainActivity.kt
+++ app/src/main/java/com/example/spaceshooters/MainActivity.kt
@@ -11,6 +11,9 @@ import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
     private lateinit var btn_play: Button
+    private lateinit var btn_options: Button
+    private lateinit var btn_credits: Button
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,6 +27,12 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
 
         btn_play = findViewById(R.id.btn_play)
         btn_play.setOnClickListener(this)
+
+        btn_options = findViewById(R.id.btn_options)
+        btn_options.setOnClickListener(this)
+
+        btn_credits = findViewById(R.id.btn_credits)
+        btn_credits.setOnClickListener(this)
     }
 
     override fun onClick(v: View?) {
@@ -31,6 +40,15 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
             val intent = Intent(this, GameActivity::class.java)
             startActivity(intent)
         }
+        if (v?.id == btn_options.id) {
+            val intent = Intent(this, OptionsActivity::class.java)
+            startActivity(intent)
+        }
+        if (v?.id == btn_credits.id) {
+            val intent = Intent(this, CreditsActivity::class.java)
+            startActivity(intent)
+        }
+    }
     }
 
 
